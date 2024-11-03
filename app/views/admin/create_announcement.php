dce9ff21b881e4d03a79178f6474a2aff144a89b
SenujaUdugampola <senuja00@gmail.com>
2024-11-03 09:49:52 +0530

    Add headers

--- app/views/admin/create_announcement.php
+++ app/views/admin/create_announcement.php
@@ -4,6 +4,7 @@
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <?php require_once APPROOT . '/views/inc/components/header.php'; ?>
       <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
       <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/components/side_panel.css">
       <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/admin/create_announcement.css">
