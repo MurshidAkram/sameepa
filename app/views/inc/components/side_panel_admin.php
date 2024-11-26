964c2d2556c4e71778236c5888b3ff097b805145
SenujaUdugampola <senuja00@gmail.com>
2024-11-26 14:08:42 +0530

    Update side_panel_admin.php

--- app/views/inc/components/side_panel_admin.php
+++ app/views/inc/components/side_panel_admin.php
@@ -2,15 +2,15 @@
 <aside>
         <ul>
                 <li><a href="<?php echo URLROOT; ?>/admin/dashboard">Dashboard</a></li>
-                <li><a href="<?php echo URLROOT; ?>/posts/index">Feed</a></li>
-                <li><a href="<?php echo URLROOT; ?>/announcements/index">Community Announcements</a></li>
+                <li><a href="<?php echo URLROOT; ?>/posts/index">Manage Feed</a></li>
+                <li><a href="<?php echo URLROOT; ?>/announcements/index">Manage Announcements</a></li>
                 <li><a href="<?php echo URLROOT; ?>/events/index">Manage Events</a></li>
                 <li><a href="<?php echo URLROOT; ?>/forums/index">Manage Forums</a></li>
-                <li><a href="<?php echo URLROOT; ?>/polls/index">Polls</a></li>
-                <li><a href="<?php echo URLROOT; ?>/chat/index">Chats</a></li>
+                <li><a href="<?php echo URLROOT; ?>/polls/index">Manage Polls</a></li>
+                <li><a href="<?php echo URLROOT; ?>/chat/index">Manage Chats</a></li>
                 <li><a href="<?php echo URLROOT; ?>/facilities/index">Manage Facilities</a></li>
-                <li><a href="<?php echo URLROOT; ?>/admin/payments">Payments</a></li>
-                <li><a href="<?php echo URLROOT; ?>/admin/complaints">Complaints</a></li>
+                <li><a href="<?php echo URLROOT; ?>/admin/payments">Manage Payments</a></li>
+                <li><a href="<?php echo URLROOT; ?>/admin/complaints">Manage Complaints</a></li>
                 <li><a href="<?php echo URLROOT; ?>/groups/index">Manage Groups</a></li>
                 <li><a href="<?php echo URLROOT; ?>/admin/exchange">Manage Exchange Center</a></li>
         </ul>
